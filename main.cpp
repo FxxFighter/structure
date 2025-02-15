e345a3daeece69ab390faf31729a2c6b7f7d284f
FxxFighter <xofighter24@gmail.com>
2025-02-16 00:32:24 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 struct Animal {
     char name[30];
@@ -9,7 +10,7 @@ struct Animal {
 };
 
 int main() {
-    struct Animal zoo[] = {
+    Animal zoo[] = {
         {"Leo", "Lion", 5, 190.5, "Carnivore"},
         {"gondebak", "Elephant", 10, 5400.0, "Herbivore"},
         {"vafa", "Dog", 3, 25.0, "Omnivore"},
@@ -20,8 +21,8 @@ int main() {
     int numAnimals = sizeof(zoo) / sizeof(zoo[0]);
 
     for (int i = 0; i < numAnimals; i++) {
-        printf("Name: %s, Species: %s, Age: %d, Weight: %.1f kg, Diet: %s\n",
-               zoo[i].name, zoo[i].species, zoo[i].age, zoo[i].weight, zoo[i].diet);
+        cout << "Name: " << zoo[i].name << ", Species: " << zoo[i].species
+             << ", Age: " << zoo[i].age << ", Weight: " << zoo[i].weight << " kg, Diet: " << zoo[i].diet << endl;
     }
 
     return 0;
